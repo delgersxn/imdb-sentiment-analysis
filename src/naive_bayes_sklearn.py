8c7061a77c5a1cadc475d0720b49403a75ed83fb
Derrek <itsmedelger@gmail.com>
2025-05-28 22:34:23 +1000

    note

--- src/naive_bayes_sklearn.py
+++ src/naive_bayes_sklearn.py
@@ -3,6 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from config import tfidf_max_features
 
+# TODO: Could add more preprocessing (for example: remove stop)
+
 
 def build_sklearn_nb():
     return Pipeline([
